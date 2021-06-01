7168b0d9392723b252211ba6022d1f0778f69778
Aakash Sharma <aakashupadhyay.25@gmail.com>
2021-06-01 19:35:28 +0530

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 v-if="true">Hello from Home Components !</h1>
+    <button v-on:click="hello" class="btn btn-primary">
+      Call Hello Function
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+  methods: {
+    hello() {
+      alert("Hello Function Called !");
+    },
+    data() {
+      return { show: false };
+    },
+  },
+};
+</script>
